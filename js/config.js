4fb2940652109407bf91e4e2f238dc2d71cf2f65
RangerRishabh <104273257+RangerRishabh@users.noreply.github.com>
2024-12-21 10:32:30 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_7ahfg9dFk', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7fkkct5rnrqtiv65oqtvuv6p9q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1', // e.g. us-east-2
-        userPoolClientSecret: '1a9timvjpqpuohvt6id8i58ll33f97gkmo9976cv69su5fchmon6'
+        userPoolClientId: '1j15aleidbtn4let043vkqbh9k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2        
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
